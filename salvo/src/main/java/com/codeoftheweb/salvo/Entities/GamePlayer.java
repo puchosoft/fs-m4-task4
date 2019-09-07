673d03f26d7b9bdd4779680809d0286e15506137
Jose Luis Gonzalez <puchosoft@gmail.com>
2019-09-07 20:22:21 -0300

    Nueva entidad y repositorio para Salvo

--- salvo/src/main/java/com/codeoftheweb/salvo/Entities/GamePlayer.java
+++ salvo/src/main/java/com/codeoftheweb/salvo/Entities/GamePlayer.java
@@ -31,6 +31,10 @@ public class GamePlayer {
   @OneToMany(mappedBy = "gamePlayer", fetch = FetchType.EAGER)
   private Set<Ship> ships;
 
+  // Relacion con la tabla "salvoes"
+  @OneToMany(mappedBy = "gamePlayer", fetch = FetchType.EAGER)
+  private Set<Salvo> salvoes;
+
   private Date joinDate;
 
   public GamePlayer() {
@@ -43,23 +47,27 @@ public class GamePlayer {
   }
 
   public long getId() {
-    return id;
+    return this.id;
   }
 
   public Game getGame() {
-    return game;
+    return this.game;
   }
 
   public Player getPlayer() {
-    return player;
+    return this.player;
   }
 
   public Date getJoinDate() {
-    return joinDate;
+    return this.joinDate;
   }
 
   public Set<Ship> getShips(){
-    return ships;
+    return this.ships;
+  }
+
+  public Set<Salvo> getSalvoes(){
+    return this.salvoes;
   }
 
   // Salida DTO para los objetos GamePlayer
--- salvo/src/main/java/com/codeoftheweb/salvo/Entities/Salvo.java
+++ salvo/src/main/java/com/codeoftheweb/salvo/Entities/Salvo.java
@@ -1,63 +1,49 @@
 package com.codeoftheweb.salvo.Entities;
 
 import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.*;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Set;
 
 @Entity
-public class Ship {
+public class Salvo {
 
-  // ID automatico para la tabla "ships"
+  // ID automatico para la tabla "salvoes"
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
   @GenericGenerator(name = "native", strategy = "native")
   private long id;
-
-  private String shipType;
 
   // Relacion con la tabla "gamePlayers"
   @ManyToOne(fetch = FetchType.EAGER)
   @JoinColumn(name = "gamePlayer_id")
   private GamePlayer gamePlayer;
 
+  private long turn;
+
   @ElementCollection
   @Column(name="location")
   private Set<String> locations = new HashSet<>();
 
-  public Ship(){
-  }
-
-  public Ship(String shipType, GamePlayer gamePlayer, Set<String> locations){
-    this.shipType = shipType;
-    this.gamePlayer = gamePlayer;
-    this.locations = locations;
+  public Salvo(){
   }
 
   public long getId() {
     return this.id;
   }
 
-  public String getShipType() {
-    return this.shipType;
-  }
-
   public GamePlayer getGamePlayer() {
     return this.gamePlayer;
   }
 
-  public Set<String> getLocations(){
-    return locations;
+  public long getTurn() {
+    return this.turn;
   }
 
-  // Salida DTO para los objetos Ship
-  public Map<String, Object> toDTO() {
-    Map<String, Object> dto = new LinkedHashMap<>();
-    dto.put("type", this.shipType);
-    dto.put("locations", this.locations);
-    return dto;
+  public Set<String> getLocations() {
+    return this.locations;
   }
+
 
 }
